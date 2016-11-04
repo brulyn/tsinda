60515596b6d67d215aaf607298b37d0b655aebbf
Bruce Higiro <higirobru@gmail.com>
2016-11-04 16:10:03 +0200

    8

--- config/env/production.js
+++ config/env/production.js
@@ -5,16 +5,16 @@ module.exports = {
     facebook: {
         clientID: '597390973774147',
         clientSecret: 'ba964a455812afb6aa16355dfc28bb94',
-        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+        callbackURL: 'https://tsinda.herokuapp.com/oauth/facebook/callback'
     },
     twitter: {
         clientID: '0dIQk2MmTibxz4rfg9XeKCUMV',
         clientSecret: 'xrKtEYrA9nD8asPx2wdQMHHv0B4BKxo1Y3BQZxQ503t0p9XZvt',
-        callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+        callbackURL: 'https://tsinda.herokuapp.com/oauth/twitter/callback'
     },
     google: {
         clientID: '578665604715-m4mhcl3msccjlhc9eabhovgvqpov022g.apps.googleusercontent.com',
         clientSecret: 'UgIq5y85nDdyIAsIhW3V8bAd',
-        callbackURL: 'http://localhost:3000/oauth/google/callback'
+        callbackURL: 'https://tsinda.herokuapp.com/oauth/google/callback'
     }
 };
