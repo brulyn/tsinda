855f3f790efbf3b13af0f5d66faa2f62c86658a8
Bruce Higiro <higirobru@gmail.com>
2016-11-06 17:38:14 +0200

    latest

--- app/models/user.js
+++ app/models/user.js
@@ -26,9 +26,7 @@ var UserSchema = new Schema({
     division: String,
     year_studies: String,
     section: String,
-    my_materials:{    
-        title: {type: String}
-    },
+    my_materials:{},
     my_tests: {    
         test_id: {type: Schema.ObjectId},
         test_performance: Number
