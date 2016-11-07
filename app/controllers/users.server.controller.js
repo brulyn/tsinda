9d32fbdcc9eb5d658f3f8b743b5b2bdb2db7b956
Bruce Higiro <higirobru@gmail.com>
2016-11-07 16:28:37 +0200

    inserting my_materials

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -159,7 +159,7 @@ exports.updateMyProfile = function (req, res, next) {
         User.findOneAndUpdate(
                 {username: req.user.username },
                 {
-                    $push: {my_materials:{$each: st_materials}}
+                    $push: {my_materials: {title: {$each: st_materials} }}
                 },
                 { upsert:true },
                 function(err, affct){
