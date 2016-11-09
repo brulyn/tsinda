f8347ed4fe4637be490dd84fd7c6079092bba3ee
Bruce Higiro <higirobru@gmail.com>
2016-11-09 14:48:21 +0200

    inserting my_materials

---
+++ app/models/progress.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ObjectId = Schema.ObjectId;
+
+var schema = new Schema({
+    user_id: {type: ObjectId},
+    user_progress: {type:Number, default:0}
+});
+
+module.exports = mongoose.model('Progress', schema);
