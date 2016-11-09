2a310fb4aa3c3db0e36d0361000354f699378c42
Bruce Higiro <higirobru@gmail.com>
2016-11-09 19:17:12 +0200

    updating my_Progress

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,6 +1,7 @@
 var passport = require('passport'),
     Materials = require('mongoose').model('Material'),
     Chapters = require('mongoose').model('Chapter'),
+    Contents = require('mongoose').model('Content'),
     Users = require('mongoose').model('User'),
     users = require('./users.server.controller');
 
@@ -21,20 +22,28 @@ exports.render = function (req, res) {
             Users.find({ section: req.user.section }).count(function(err, n_us){
                 Materials.find( { title: {$in: req.user.my_materials}}, function(err, materials_list){
                     for(var i=0; i<materials_list.length; i++){
-                        my_materials_ids.push(materials_list._id);
+                        my_materials_ids.push(materials_list[i]._id);
                     }
-                    Chapters.find( {material_id: {$in: my_materials_ids}, done:false}).count(function(err, number_chapters){
-                        req.app.locals.number_chapters = number_chapters;
-                        res.render(
-                        'index', {
-                            title: 'Tsinda Platform',
-                            user: req.user,
-                            date: req.user.created.toDateString(),
-                            number_materials: n,
-                            number_mates: n_us,
-                            number_chapters: number_chapters,
-                            img_url: (req.user.provider == 'facebook') ? req.user.providerData.picture.data.url :req.user.providerData.image.url
-                        });
+                    Chapters.find( {material_id: {$in: my_materials_ids}}, function(err, chapters_list){
+                        var number_chapters = 0;
+                        var chapters_ids = [];
+                        for(var i=0; i< chapters_list.length; i++){
+                            number_chapters++;
+                            chapters_ids.push(chapters_list[i]._id);
+                        }
+                        Contents.find({ chapter: {$in: chapters_ids} }).count(function(err, number_contents){
+                            res.render(
+                            'index', {
+                                title: 'Tsinda Platform',
+                                user: req.user,
+                                date: req.user.created.toDateString(),
+                                number_materials: n,
+                                number_mates: n_us,
+                                number_pages: number_contents,
+                                img_url: (req.user.provider == 'facebook') ? req.user.providerData.picture.data.url :req.user.providerData.image.url
+                            });
+                        })
+                        
                     });
                     
                 });
