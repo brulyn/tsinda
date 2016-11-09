b45d00527d633431e9fe038ac919cef86919615b
Bruce Higiro <higirobru@gmail.com>
2016-11-09 11:47:06 +0200

    inserting my_materials

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,5 +1,6 @@
 var passport = require('passport'),
     Materials = require('mongoose').model('Material'),
+    Users = require('mongoose').model('User'),
     users = require('./users.server.controller');
 
 exports.render = function (req, res) {
@@ -15,22 +16,24 @@ exports.render = function (req, res) {
         var image_url = "";
         Materials.find({ title: { $in: req.user.my_materials } }).count(function(err, n){
             req.app.locals.number_materials = n;
-            res.render(
-            'index', {
-                title: 'Tsinda Platform',
-                user: req.user,
-                date: req.user.created.toDateString(),
-                number_materials: req.app.locals.number_materials,
-                img_url: (req.user.provider == 'facebook') ? req.user.providerData.picture.data.url :req.user.providerData.image.url
-            }
-        );
+            Users.find({ section: req.user.section }).count(function(err, n_us){
+                res.render(
+                'index', {
+                    title: 'Tsinda Platform',
+                    user: req.user,
+                    date: req.user.created.toDateString(),
+                    number_materials: n,
+                    number_mates: n_us,
+                    img_url: (req.user.provider == 'facebook') ? req.user.providerData.picture.data.url :req.user.providerData.image.url
+                });    
+            })
+            
         });
         
         users.myMaterials();
     } else {
 
         res.render(
-
             'login', {
                 title: 'Sign-in Form',
                 layout: false
