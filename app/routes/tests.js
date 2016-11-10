b0eb191ce9ef692b080085afae988fb62152f91a
Bruce Higiro <higirobru@gmail.com>
2016-11-10 14:59:04 +0200

    working on tests

--- app/routes/tests.js
+++ app/routes/tests.js
@@ -2,5 +2,5 @@ module.exports = function (app) {
     var tests = require('../controllers/tests.server.controller');
     app.get('/tests', tests.render);
     app.get('/test/:id', tests.renderTest);
-    app.get('/test/done/:id', tests.renderResults);
+    app.post('/test/done/:id', tests.renderResults);
 };
