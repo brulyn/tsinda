e8a3704e6187458260844087da7501f09f3be1dd
Bruce Higiro <higirobru@gmail.com>
2016-11-10 14:03:10 +0200

    updating content status to done

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -34,7 +34,7 @@ exports.render = function (req, res) {
                         Contents.find({chapter: {$in: chapters_list}}).count(function(err, n_contents){
 
                             Contents.find({ done: {_id: req.user._id} }).count(function(err, number_contents){  
-                                var percent = ((number_contents/n_contents,2).toFixed(2))*100 ;                          
+                                var percent = ((number_contents/n_contents,2).toFixed(2)) ;                          
                                 res.render(
                                 'index', {
                                     title: 'Tsinda Platform',
