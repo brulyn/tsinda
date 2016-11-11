9a0a8a52569302e50ecf1d010e8d418ac55cbd96
Bruce Higiro <higirobru@gmail.com>
2016-11-11 11:18:09 +0200

    working on tests

--- app/models/test.js
+++ app/models/test.js
@@ -5,16 +5,18 @@ var ObjectId = Schema.ObjectId;
 var schema = new Schema({
     desc: { type: String, trim: true },
     n_questions: { Number },
-    answer1: String,
-    answer2: String,
-    answer3: String,
-    answer4: String,
-    answer5: String,
-    answer6: String,
-    answer7: String,
-    answer8: String,
-    answer9: String,
-    answer10: String
+    answers:[{
+        answer1: String,
+        answer2: String,
+        answer3: String,
+        answer4: String,
+        answer5: String,
+        answer6: String,
+        answer7: String,
+        answer8: String,
+        answer9: String,
+        answer10: String
+    }]
     
 });
 
